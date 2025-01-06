8d039a0037336fd1d3dcb664ea0a5b66f29557b9
Matt Romanes <48772682+romanesmatt@users.noreply.github.com>
2025-01-06 19:47:34 +1300

    Add files to gitignore

--- obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net9.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cf66d88de7b96bef35e2241bbe559c71822343")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
